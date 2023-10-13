5159759da5d1d5adb013bd98b24bef817f11c7fa
jason <zzhou292@wisc.edu>
2023-10-13 14:00:34 -0500

    started working on unitree go 1 parser

---
+++ src/demos/parsers/demo_PARSER_URDF_Unitree.cpp
@@ -0,0 +1,181 @@
+// =============================================================================
+// PROJECT CHRONO - http://projectchrono.org
+//
+// Copyright (c) 2023 projectchrono.org
+// All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file at the top level of the distribution and at
+// http://projectchrono.org/license-chrono.txt.
+//
+// =============================================================================
+// Authors: Radu Serban
+// =============================================================================
+//
+// Demo for the URDF -> Chrono parser
+//
+// =============================================================================
+
+////#include <float.h>
+////unsigned int fp_control_state = _controlfp(_EM_INEXACT, _MCW_EM);
+
+#include "chrono/physics/ChBodyEasy.h"
+#include "chrono/physics/ChSystemNSC.h"
+#include "chrono/physics/ChSystemSMC.h"
+#include "chrono/core/ChRealtimeStep.h"
+#include "chrono/assets/ChBoxShape.h"
+
+#include "chrono_parsers/ChParserURDF.h"
+#include "chrono_parsers/ChRobotActuation.h"
+
+#include "chrono/assets/ChVisualSystem.h"
+#ifdef CHRONO_IRRLICHT
+    #include "chrono_irrlicht/ChVisualSystemIrrlicht.h"
+using namespace chrono::irrlicht;
+#endif
+#ifdef CHRONO_VSG
+    #include "chrono_vsg/ChVisualSystemVSG.h"
+using namespace chrono::vsg3d;
+#endif
+
+using namespace chrono;
+using namespace chrono::parsers;
+
+// -----------------------------------------------------------------------------
+
+ChVisualSystem::Type vis_type = ChVisualSystem::Type::IRRLICHT;
+
+// -----------------------------------------------------------------------------
+
+std::shared_ptr<ChBody> CreateTerrain(ChSystem& sys, double length, double width, double height, double offset) {
+    float friction = 0.8f;
+    float Y = 1e7f;
+    float cr = 0.0f;
+
+    auto ground_mat = ChMaterialSurface::DefaultMaterial(sys.GetContactMethod());
+    ground_mat->SetFriction(friction);
+    ground_mat->SetRestitution(cr);
+
+    if (sys.GetContactMethod() == ChContactMethod::SMC) {
+        std::static_pointer_cast<ChMaterialSurfaceSMC>(ground_mat)->SetYoungModulus(Y);
+    }
+
+    auto ground = std::shared_ptr<ChBody>(sys.NewBody());
+    ground->SetBodyFixed(true);
+    ground->SetPos(ChVector<>(offset, 0, height - 0.1));
+    ground->SetCollide(true);
+
+    ground->GetCollisionModel()->ClearModel();
+    ground->GetCollisionModel()->AddBox(ground_mat, length, width, 0.2);
+    ground->GetCollisionModel()->BuildModel();
+
+    auto box = chrono_types::make_shared<ChBoxShape>(length, width, 0.2);
+    box->SetTexture(GetChronoDataFile("textures/checker2.png"), (float)length, (float)width);
+    ground->AddVisualShape(box);
+
+    sys.AddBody(ground);
+
+    return ground;
+}
+
+// -----------------------------------------------------------------------------
+
+int main(int argc, char* argv[]) {
+    // Create a Chrono system
+    ChSystemSMC sys;
+    sys.Set_G_acc(ChVector<>(0, 0, -9.8));
+
+    // Create parser instance
+    ChParserURDF robot(GetChronoDataFile("robot/unitree-go1/urdf/go1.urdf"));
+
+    // Set root body pose
+    robot.SetRootInitPose(ChFrame<>(ChVector<>(0, 0, 1.5), QUNIT));
+
+    // Report parsed elements
+    robot.PrintModelBodies();
+    robot.PrintModelJoints();
+
+    // Create the Chrono model
+    robot.PopulateSystem(sys);
+
+    // Fix root body
+    robot.GetRootChBody()->SetBodyFixed(true);
+
+    // Create the visualization window
+    std::shared_ptr<ChVisualSystem> vis;
+    auto camera_lookat = robot.GetRootChBody()->GetPos();
+    auto camera_loc = camera_lookat + ChVector<>(3, 3, 0);
+#ifndef CHRONO_IRRLICHT
+    if (vis_type == ChVisualSystem::Type::IRRLICHT)
+        vis_type = ChVisualSystem::Type::VSG;
+#endif
+#ifndef CHRONO_VSG
+    if (vis_type == ChVisualSystem::Type::VSG)
+        vis_type = ChVisualSystem::Type::IRRLICHT;
+#endif
+
+    switch (vis_type) {
+        case ChVisualSystem::Type::IRRLICHT: {
+#ifdef CHRONO_IRRLICHT
+            auto vis_irr = chrono_types::make_shared<ChVisualSystemIrrlicht>();
+            vis_irr->AttachSystem(&sys);
+            vis_irr->SetCameraVertical(CameraVerticalDir::Z);
+            vis_irr->SetWindowSize(1200, 800);
+            vis_irr->SetWindowTitle("RoboSimian URDF demo");
+            vis_irr->Initialize();
+            vis_irr->AddLogo();
+            vis_irr->AddSkyBox();
+            vis_irr->AddCamera(camera_loc, camera_lookat);
+            vis_irr->AddTypicalLights();
+
+            vis = vis_irr;
+#endif
+            break;
+        }
+        default:
+        case ChVisualSystem::Type::VSG: {
+#ifdef CHRONO_VSG
+            auto vis_vsg = chrono_types::make_shared<ChVisualSystemVSG>();
+            vis_vsg->AttachSystem(&sys);
+            vis_vsg->SetCameraVertical(CameraVerticalDir::Z);
+            vis_vsg->SetWindowTitle("RoboSimian URDF demo");
+            vis_vsg->AddCamera(camera_loc, camera_lookat);
+            vis_vsg->SetWindowSize(ChVector2<int>(1200, 800));
+            vis_vsg->SetWindowPosition(ChVector2<int>(400, 100));
+            vis_vsg->SetClearColor(ChColor(0.455f, 0.525f, 0.640f));
+            vis_vsg->SetUseSkyBox(false);
+            vis_vsg->SetCameraAngleDeg(40.0);
+            vis_vsg->SetLightIntensity(0.5f);
+            vis_vsg->SetLightDirection(1.5 * CH_C_PI_2, CH_C_PI_4);
+            vis_vsg->SetWireFrameMode(false);
+            vis_vsg->Initialize();
+
+            vis = vis_vsg;
+#endif
+            break;
+        }
+    }
+
+    // Solver settings
+    sys.SetSolverMaxIterations(200);
+    sys.SetSolverType(ChSolver::Type::BARZILAIBORWEIN);
+
+    // Simulation loop
+    double step_size = 5e-4;
+    ChRealtimeStepTimer real_timer;
+    bool terrain_created = false;
+
+    while (vis->Run()) {
+        double time = sys.GetChTime();
+
+        vis->BeginScene();
+        vis->Render();
+        vis->EndScene();
+
+        // Advance system dynamics
+        sys.DoStepDynamics(step_size);
+        real_timer.Spin(step_size);
+    }
+
+    return 0;
+}
