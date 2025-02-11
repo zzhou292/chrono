748fd298a3fec0475a5103d00270fae3797026e7
zzhou292@wisc.edu <zzhou292@wisc.edu>
2025-02-11 15:30:53 -0600

    api fix

--- src/demos/synchrono/dds/demo_SYN_DDS_distributed.cpp
+++ src/demos/synchrono/dds/demo_SYN_DDS_distributed.cpp
@@ -324,12 +324,12 @@ int main(int argc, char* argv[]) {
     if (node_id % 2 == 0) {
         // Start even vehicles in a row on the south side, driving north
         init_loc = ChVector3d(0, 2.0 * (node_id + 1), 0.5);
-        init_rot = Q_from_AngZ(0);
+        init_rot = QuatFromAngleZ(0);
         path = StraightLinePath(init_loc, init_loc + ChVector3d(pathLength, 0, 0));
     } else {
         // Start odd vehicles staggered going up the west edge, driving east
         init_loc = ChVector3d(2.0 * (node_id - 1), -5.0 - 2.0 * (node_id - 1), 0.5);
-        init_rot = Q_from_AngZ(CH_PI / 2);
+        init_rot = QuatFromAngleZ(CH_PI / 2);
         path = StraightLinePath(init_loc, init_loc + ChVector3d(0, pathLength, 0));
     }
     ChPathFollowerDriver driver(vehicle, path, "Box path", target_speed);
@@ -358,7 +358,7 @@ int main(int argc, char* argv[]) {
         if (time >= end_time)
             break;
 
-            // Render scene
+        // Render scene
 #ifdef CHRONO_IRRLICHT
         if (step_number % render_steps == 0)
             app.Render();
